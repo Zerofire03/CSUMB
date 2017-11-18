b997cebed804ddc88c5acf1c23fe1de4faf82963
Christopher Holmes <Zerofire03@users.noreply.github.com>
2017-11-17 20:01:39 -0800

    Lab8

--- CST205/Module3/Midterm.py
+++ CST205/Module3/Midterm.py
@@ -10,3 +10,62 @@ def pyCopyBlack(source, target, targetX, targetY):
       if color.getRed() == 0 and color.getGreen() == 0 and color.getBlue() == 0:
         setColor(getPixel(target, (targetX + x), (targetY + y)), color)
   return target
+  
+def chromakey():
+  pic =  makePicture(pickAFile())
+  picBG =  makePicture(pickAFile())
+  for x in range(0, getWidth(pic)):
+    for y in range(0, getHeight(pic)):
+      px = getPixel(pic, x, y)
+      pxBG = getPixel(picBG, x, y)
+      color = getColor(px)
+      colorBG = getColor(pxBG)
+      if distance(color, white) < 200.0:
+        setColor(px, colorBG) 
+  show(pic)
+  file = r"/Users/chris/Documents/GitHub/CSUMB/CST205/Module3/portfolio/mergedColorArtify.jpg"
+  writePictureTo(pic, file)
+  
+def artify():
+  # Current color range	New value for color
+  #color < 64	31
+  #63 < color < 128	95
+  #127 < color < 192	159
+  #191 < color < 256	223
+  pic =  makePicture(pickAFile())
+  
+  for px in getPixels(pic):
+    R = getRed(px) 
+    B = getBlue(px)
+    G = getGreen(px)
+    
+    # red
+    if R < 64:
+      setRed(px, 31)
+    elif R >= 64 and R < 128:
+      setRed(px, 95)
+    elif R >= 128 and R < 192:
+      setRed(px, 159)
+    elif R >= 192 and R < 256:
+      setRed(px, 223)
+
+    # blue
+    if B < 64:
+      setBlue(px, 31)
+    elif B >= 64 and B < 128:
+      setBlue(px, 95)
+    elif B >= 128 and B < 192:
+      setBlue(px, 159)
+    elif B >= 192 and B < 256:
+      setBlue(px, 223)
+    
+    # green
+    if G < 64:
+      setGreen(px, 31)
+    elif G >= 64 and G < 128:
+      setGreen(px, 95)
+    elif G >= 128 and G < 192:
+      setGreen(px, 159)
+    elif G >= 192 and G < 256:
+      setGreen(px, 223)
+  show(pic)
---
+++ CST205/Module4/Lab8/Lab8.py
@@ -0,0 +1,33 @@
+#sound = makeSound(pickAFile())
+
+def increaseVolume(sound):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value * 2)
+    
+#Decreases the volume of a sound by half
+def decreaseVolume(sound):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value / 2)
+    
+#Takes in two parameters, sound and change
+#Sound is the sound file that you want to change the volume on
+#Change is the amount you want to change by. Use fraction to decrease, whole numbers to increase
+def changeVolume(sound, change):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value * change)
+  
+#Finds the largest sample in a sound and returns it
+#sets the variable max to the sample value at the first sample
+#Loops through all samples, comparing to the value in max. 
+#If the value in the loop is larger than max, new value of max is value in loop
+def maxSample():
+  sound = makeSound(pickAFile())
+  samples = getSamples(sound)
+  max = getSampleValue(samples[0])
+  for x in range(1, len(samples)-1):
+    if getSampleValue(samples[x]) > max:
+      max = getSampleValue(samples[x])
+  return max
