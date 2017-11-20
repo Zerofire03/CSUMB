26143e76570ca33876e974fa0e93d7291baee4ea
Christopher Holmes <Zerofire03@users.noreply.github.com>
2017-11-20 15:19:09 -0800

    Midterm Progress

--- CST205/Module4/Lab10/Lab10.py
+++ CST205/Module4/Lab10/Lab10.py
@@ -1,48 +1,88 @@
-from sys import exit
-#name = requestString("What is your name?")
+#Lab 10
+#Chris Holmes
+#Chris Buckey
+#Patrick Gonzalez
 
-#while True:
-#  name = requestString("What is your name?")
-#  if name == 'stop':
-#    break
+def WarmUp():
+  name = requestString("What is your name?")
+  print name
+  
+  while True:
+    string = requestString("Enter a string:")
+    if string == "stop":
+      break
+    print string
+#####################################################################    
     
 def hangMan():
-  theWord = "socal"
+  print "Lets Play Hangman"
+  print "There is a secert word you have to guess."
+  print "You have 6 chances to guess one of the letters in the seceret word."
+  print "Good Luck!"
+  
+  
+  #This array holds all the guessed letters
   incorrect = []
-
-  print("Insert how to play hangman here")
+  #This variable counts the number of attempts
+  attempts = 6
   
-  blankWord = ['_'] * len(theWord)
-  count=0
+  #The secret word to be guessed
+  theWord = "socal"
+ 
+  #An array of blank Spaces
+  # That will show the correct guesses
+  #blankWord = [u'\u005F']*len(theWord)
+  blankWord = ['_']*len(theWord)
   
-  while count < 6:
-    print "The count is %s:" %(count)
-    print "There are %s blank spaces." %(len(blankWord))
+  #This loop asks the user for the guessed letter
+  # keeps looping until the user has reached the alloted number of guess attempts
+  while attempts > 0:
+    print "You have %s guesses" %attempts
+    print"Here is what you got so far:"
     print blankWord
-    if count > 0:
-      print "Here is what you have guessed so far:"
-      print incorrect
-    letter = requestString("Guess a letter: ").lower()
-    while true:
-      if letter.isalpha() == False or letter in incorrect or len(letter) != 1:
-        letter = requestString("Letter not valid. Please try again:").lower()
-      else:
-        break
     
-    if letter in theWord:
-      for i, elm in enumerate(theWord):
-        if letter == elm:
-          blankWord[i] = letter
+    #Ask for a guessed letter from the user 
+    guess = requestString("Guess a letter: ").lower()
+    while true:
+       #Check if the guess is a letter
+       if guess.isalpha() == False:
+           guess = requestString("Letters only.  Please try again: ").lower()
+       #Check if the guess has been guessed already
+       if guess in incorrect:
+         guess = requestString("You guessed that already.  Please try again: ").lower()
+       #Check if the user enters one letter at a time
+       if len(guess) !=1:
+         guess = requestString("Only one at a time.  Please try again: ").lower()
+       else:
+         break
+    
+    #Is the guess letter in the seceret word
+    if guess in theWord:
+      #Then find the index of the correct guess in the seceret word
+      for i, correct in enumerate(theWord):
+        #When you find the index of where the guess letter belongs in the secerte word
+        if guess == correct:
+          #Then assign the guessed letter to the matching index of the secerte word to 
+          #  that of the blankWord array to show to the user what they have guessed correctly 
+          blankWord[i] = guess 
+          #Turns the array of charecters into a string to check agaisnt the secret word
           temp = ''.join(blankWord)
-          print temp
+          #Checks to see if the user has guessed all the letters so far
           if temp == theWord:
-            print("Congratulations you won!")
-            count = 6
+            return "That is right, the seceret word is %s!" %(temp)
+    #When the user guess the letter wrong:
+    #  attach the incorrect guessed letter to the incorrect array
+    #  decrease attempt to let the user know how many attempts they have left 
     else:
-      print("Incorrect guess")
-      count = count + 1
-      incorrect.append(letter)
-      if count == 6:
-        print "HangMan! You loose!"
-      
+        incorrect.append(guess)
+        attempts -=1
+        print "You guessed: "
+        print incorrect
+  print "HangMan!"
+   
+        
+         
+        
   
+  
+
