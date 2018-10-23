5f02d564a74ab2c93e6a42015ebe9d833a6e5711
Christopher Holmes <zerofire03@users.noreply.github.com>
2018-10-23 10:48:51 -0700

    Update CST325/Week 1/Sphere/math/sphere.js
    3 tests passed. Need to finish determining if ray intersects.

--- CST325/Week 1/Sphere/math/sphere.js
+++ CST325/Week 1/Sphere/math/sphere.js
@@ -13,9 +13,41 @@ var Sphere = function(origin, radius) {
 	this.raycast = function(ray) {
 		// todo determine whether the ray intersects this sphere and where
 		
-		//console.log(ray.direction);
-		var c = origin;
-		var raydir = ray.direction;
+		
+		var L = (ray.origin).subtract(origin);
+		var tc = (L).dot(ray.direction)
+		
+		//Check to see if ray intersects sphere
+		if(tc < 0)
+		{
+			var result = { hit: false, point: null };
+			return result;
+		}
+		//console.log(L.lengthSqr());
+		var d = Math.sqrt(Math.pow(tc,2)-L.lengthSqr());
+		if(d > radius)
+		{
+			var result = { hit: false, point: null };
+			return result;
+		}
+		
+		var t1c = Math.sqrt(Math.pow(radius,2)-Math.pow(d,2));
+		var t1 = tc - t1c;
+		var t2 = tc + t1c;
+		//console.log((ray.origin).add((ray.direction).multiplyScalar(t1)));
+		//console.log((ray.origin).add((ray.direction).multiplyScalar(t2)));
+		var p1 = (ray.origin).add((ray.direction).multiplyScalar(t1));
+		var p2 = (ray.origin).add((ray.direction).multiplyScalar(t2));
+		console.log(p1);
+		console.log(p2);
+		
+		
+		var result = {
+			hit: true,
+			point: p1,
+			normal: p1.normalized(),
+			distance: p1.length()
+		};
 		
 		// Recommended steps
 		// 1. review slides/book math
