fbf424ff7533806592f928c40ba7181b1dd4b2cc
Christopher Holmes <Zerofire03@users.noreply.github.com>
2018-10-29 22:31:17 -0700

    Update CST325/Week 1/Sphere/math/sphere.js
    All 8 tests passing

--- CST325/Week 1/Sphere/math/sphere.js
+++ CST325/Week 1/Sphere/math/sphere.js
@@ -1,6 +1,3 @@
-
-
-
 var Sphere = function(origin, radius) {
 	this.origin = origin || new Vector3(0,0,0);
 	this.radius = radius || 1;
@@ -14,16 +11,17 @@ var Sphere = function(origin, radius) {
 		// todo determine whether the ray intersects this sphere and where
 		
 		//Distance from ray origin to sphere center
-		var L = (origin).subtract(ray.origin);
-		console.log("L:");
-		console.log(L);
+		//var L = (origin).subtract(ray.origin);
+		var L = this.origin.clone().subtract(ray.origin);
+		//console.log("L:");
+		//console.log(L);
 		
 		//Distance from potential point on sphere to d
 		var tc = (L).dot(ray.direction)
-		console.log("tc:", tc);
+		//console.log("tc:", tc);
 		
 		//Check to see if ray intersects sphere. If it doesn't return false
-		if(tc < 0)
+		if(tc <= 0)
 		{
 			var result = { hit: false, point: null };
 			return result;
@@ -31,7 +29,7 @@ var Sphere = function(origin, radius) {
 		
 		//Solve for distance between ray path and center of sphere
 		var d = Math.sqrt(L.dot(L)-Math.pow(tc,2));
-		console.log("d:", d);
+		//console.log("d:", d);
 		
 		
 		//If distance is greater than radius, ray does not intersect sphere
@@ -44,32 +42,32 @@ var Sphere = function(origin, radius) {
 		
 		//Distance between point of intersection on sphere and d
 		var t1c = Math.sqrt(Math.pow(radius,2)-Math.pow(d,2));
-		console.log("t1C:", t1c);
+		//console.log("t1C:", t1c);
 		
 		//Distance between ray origin and first potential intersection
 		var t1 = tc - t1c;
-		console.log("t1:", t1);
+		//console.log("t1:", t1);
 		
 		//Distance between ray origin and second potential intersection
 		var t2 = tc + t1c;
-		console.log("t2:", t2);
+		//console.log("t2:", t2);
 		
 		//Vector of first potential intersection
 		var scalar1 = (ray.direction).clone();
 		var origin1 = (ray.origin).clone()
 		var p1 = (origin1).add((scalar1).multiplyScalar(t1));
-		console.log("p1:");
-		console.log(p1);
+		//console.log("p1:");
+		//console.log(p1);
 		
 		//Vector of second potential intersection
 		var scalar2 = (ray.direction).clone();
 		var origin2 = (ray.origin).clone();
 		var p2 = (origin2).add((scalar2).multiplyScalar(t2));
-		console.log("p2:");
-		console.log(p2);
+		//console.log("p2:");
+		//console.log(p2);
 		
-		console.log("Normalized");
-		console.log(p1.normalized());
+		//console.log("Normalized");
+		//console.log(p1.normalized());
 		
 		if(t1 < t2)
 		{
@@ -79,6 +77,7 @@ var Sphere = function(origin, radius) {
 				normal: p1.normalized(),
 				distance: t1
 			};
+			return result;
 		}
 		else
 		{
@@ -88,6 +87,7 @@ var Sphere = function(origin, radius) {
 				normal: p2.normalized(),
 				distance: t2
 			};
+			return result;
 		}
 		
 		
@@ -117,6 +117,6 @@ var Sphere = function(origin, radius) {
 		//	distance: ?
 		//};
 
-		return result;
+		//return result;
 	}
 };
