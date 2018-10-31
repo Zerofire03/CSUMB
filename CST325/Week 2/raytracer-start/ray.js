467eaad58b0d446084779f08918148f6806bff24
Christopher Holmes <Zerofire03@users.noreply.github.com>
2018-10-30 22:15:29 -0700

    Removed excess folders

--- CST325/Week 2/raytracer-start/ray.js
+++
@@ -1,9 +0,0 @@
-
-var Ray = function(origin, direction) {
-	this.origin = origin;
-	this.direction = direction.normalized();
-
-	this.clone = function() {
-		return new Ray(this.origin.clone(), this.direction.clone());
-	};
-};
