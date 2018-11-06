bba71e767a1a2a0075b9165f508c2bfbe5a5e965
Christopher Holmes <zerofire03@users.noreply.github.com>
2018-11-06 13:24:33 -0800

    Added instructor files
    Added sphere and vector file from instructor.

--- CST325/Week 2/RayTracer/sphere.js
+++ CST325/Week 2/RayTracer/sphere.js
@@ -1,107 +1,36 @@
 
-
-
-var Sphere = function(origin, radius) {
-	this.origin = origin || new Vector3(0,0,0);
-	this.radius = radius || 1;
-
-	// todo - make sure origin and radius are replaced with default values if and only
-	//        if they are invalid(i.e. origin & radius should be Vector3) or undefined
-	//        default origin should be zero vector
-	//        default radius should be 1
+var Sphere = function(origin, radius, color, reflectivity) {
+	this.origin = origin;
+	this.radius = radius;
+	this.color = color || new Vector3(1, 1, 1);
+	this.reflectivity = (reflectivity != undefined) ? reflectivity: 0;
 
 	this.raycast = function(ray) {
-		// todo determine whether the ray intersects this sphere and where
+		var sphereOriginToRayOrigin = ray.origin.clone().subtract(this.origin);
 
-		//Distance from ray origin to sphere center
-		var L = (origin).subtract(ray.origin);
+		var a = ray.direction.dot(ray.direction);
+		var b = ray.direction.clone().multiplyScalar(2).dot(sphereOriginToRayOrigin);
+		var c = sphereOriginToRayOrigin.dot(sphereOriginToRayOrigin) - this.radius * this.radius;
 
-		//Distance from potential point on sphere to d
-		var tc = (L).dot(ray.direction)
+		var result = { hit: false, point: null };
 
-		//Check to see if ray intersects sphere. If it doesn't return false
-		if(tc < 0)
-		{
-			var result = { hit: false, point: null };
+		var discriminant = b * b - 4 * a * c;
+		if (discriminant < 0) {
 			return result;
 		}
 
-		//Solve for distance between ray path and center of sphere
-		var d = Math.sqrt(L.dot(L)-Math.pow(tc,2));
+		var t1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		var t2 = (-b + Math.sqrt(discriminant)) / (2 * a);
 
+		if (t1 > 0 && t2 > 0) {
+			var t = (t1 < t2) ? t1: t2;
+			var originOffset = ray.clone().direction.multiplyScalar(t);
 
-		//If distance is greater than radius, ray does not intersect sphere
-		if(d > radius || d < 0)
-		{
-			console.log("Radius: ", radius);
-			var result = { hit: false, point: null };
-			return result;
+			result.hit = true;
+			result.point = ray.origin.clone().add(originOffset);
+			result.normal = (result.point.clone().subtract(this.origin)).normalized();
+			result.distance = originOffset.length();
 		}
-
-		//Distance between point of intersection on sphere and d
-		var t1c = Math.sqrt(Math.pow(radius,2)-Math.pow(d,2));
-
-		//Distance between ray origin and first potential intersection
-		var t1 = tc - t1c;
-
-		//Distance between ray origin and second potential intersection
-		var t2 = tc + t1c;
-
-		//Vector of first potential intersection
-		var scalar1 = (ray.direction).clone();
-		var origin1 = (ray.origin).clone()
-		var p1 = (origin1).add((scalar1).multiplyScalar(t1));
-
-		//Vector of second potential intersection
-		var scalar2 = (ray.direction).clone();
-		var origin2 = (ray.origin).clone();
-		var p2 = (origin2).add((scalar2).multiplyScalar(t2));
-
-		if(t1 < t2)
-		{
-			var result = {
-				hit: true,
-				point: p1,
-				normal: p1.normalized(),
-				distance: t1
-			};
-		}
-		else
-		{
-			var result = {
-				hit: true,
-				point: p2,
-				normal: p2.normalized(),
-				distance: t2
-			};
-		}
-
-
-
-		// Recommended steps
-		// 1. review slides/book math
-		// 2. create the vector(s) needed to solve for the coefficients in the
-		//    quadratic equation
-		// 3. calculate the discriminant
-		// 4. use the result of the discriminant to determine if further computation
-		//    is necessary
-		// 5. return the following javascript object literal with the following properties
-		//		case 1: no VALID intersections
-		//	      var result = { hit: false, point: null }
-		//		case 2: 1 or more intersections
-		//			var result = {
-		//        		hit: true,
-		// 				point: 'a Vector3 containing the closest VALID intersection',
-		//              normal: 'a vector3 containing a unit length normal at the intersection',
-		//              distance: 'a scalar containing the intersection distance from the ray origin'
-		//          }
-
-		//var result = {
-		//	hit: ?,
-		//	point: ?,
-		//	normal: ?,
-		//	distance: ?
-		//};
 
 		return result;
 	}
--- CST325/Week 2/RayTracer/vector3.js
+++ CST325/Week 2/RayTracer/vector3.js
@@ -3,14 +3,14 @@
  */
 
 var Vector3 = function(x, y, z) {
-	this.x = x || 0; this.y = y || 0; this.z = z || 0;
+	this.x = x; this.y = y; this.z = z;
 
-	// todo - make sure to set a default value in case x, y, or z is not passed in
+	if (this.x == undefined) {
+		this.x = this.y = this.z = 0;
+	}
 
 	this.set = function(x, y, z) {
-		//todo set this' values to those from x, y, and z
 		this.x = x; this.y = y; this.z = z;
-		return this;
 	}
 
 	this.clone = function() {
@@ -18,86 +18,67 @@ var Vector3 = function(x, y, z) {
 	};
 
 	this.copy = function(other) {
-		// copy the values from other into this
 		this.x = other.x; this.y = other.y; this.z = other.z;
 		return this;
 	}
 
-	this.add = function(v) {
-		// todo - add v to this vector (1pt)
-		// This SHOULD change the values of this.x, this.y, and this.z
+	this.add = function(v) {		
 		this.x += v.x; this.y += v.y; this.z += v.z;
 		return this;
 	};
 
 	this.subtract = function(v) {
-		// todo - subtract v from this vector (1pt)
-		// This SHOULD change the values of this.x, this.y, and this.z
 		this.x -= v.x; this.y -= v.y; this.z -= v.z;
 		return this;
 	};
 
 	this.negate = function() {
-		// multiply this vector by -1 (1pt)
-		// This SHOULD change the values of this.x, this.y, and this.z
 		this.x = -this.x; this.y = -this.y; this.z = -this.z;
 		return this;
 	};
 
-	this.multiplyScalar = function(scalar) {
-		// multiply this vector by "scalar" (1pt)
-		// This SHOULD change the values of this.x, this.y, and this.z
-		this.x *= scalar; this.y *= scalar; this.z *= scalar;
-		return this;
-	};
-
 	this.length = function() {
-		// todo - return the magnitude (a.k.a length) of this vector (1pt)
-		// This should NOT change the values of this.x, this.y, and this.z
-		return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
-		//return 0;
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
 	};
 
 	this.lengthSqr = function() {
-		// todo - return the squared magnitude of this vector ||v||^2 (1pt)
-		// This should NOT change the values of this.x, this.y, and this.z
-		return (Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
-		return 0;
+		return this.x * this.x + this.y * this.y + this.z * this.z;
+	}
+
+	this.multiplyScalar = function(scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+		this.z *= scalar;
+		return this;
 	};
 
-	this.normalized = function() {
-		// todo - return a new vector that is a normalized version of this vector (1pt)
-		// This should NOT change the values of this.x, this.y, and this.z
-		//return null; // Should return a new vector that is not this
-		var length = Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
-		var newx = this.x/length
-		var newy = this.y/length
-		var newz = this.z/length
-		return new Vector3(newx, newy, newz);
+	this.normalized = function() {		
+		return new Vector3(this.x, this.y, this.z).multiplyScalar(1 / this.length());
 	};
 
 	this.normalize = function() {
-		// todo - Change the components of this vector so that its magnitude will equal 1. (1pt)
-		// This SHOULD change the values of this.x, this.y, and this.z
-		var length =  Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
-		this.x/=length 
-		this.y/=length
-		this.z/=length
+		var magnitude = this.length();
+		this.x = this.x / magnitude;
+		this.y = this.y / magnitude;
+		this.z = this.z / magnitude;
 		return this;
 	};
 
 	this.dot = function(other) {
-		// todo - return the dot product betweent this vector and "other" (5pt)
-		// This should NOT change the values of this.x, this.y, and this.z
-		return (this.x*other.x) + (this.y*other.y) + (this.z*other.z);
+		return this.x * other.x + this.y * other.y + this.z * other.z;
 	};
 
 	this.cross = function(other) {
-		// todo - return the cross product (as a new vector) betweent this vector and "other" (3pt)
-		// This should NOT change the values of this.x, this.y, and this.z
-		var cx = (this.y)*(other.z) - (this.z)*(other.y);
-		var cy = (this.z)*(other.x) - (this.x)*(other.z);
-		var cz = (this.x)*(other.y) - (this.y)*(other.x);
-		return new Vector3(cx, cy, cz);
+		return new Vector3(
+			this.y * other.z - this.z * other.y, 
+			this.z * other.x - this.x * other.z,
+			this.x * other.y - this.y * other.x
+		);
+	};
+
+	this.mulitplyVector = function(other) {
+		this.x *= other.x;
+		this.y *= other.y;
+		this.z *= other.z;
 	}
 };
