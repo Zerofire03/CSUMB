8eab6377c7df4220926025fe874590ee7632d196
Christopher Holmes <Zerofire03@users.noreply.github.com>
2019-02-11 21:53:02 -0800

    Module 5 Complete

--- CST370/Module5/ProgramAssignment/1/selectionsort.cpp
+++ CST370/Module5/ProgramAssignment/1/selectionsort.cpp
@@ -1,6 +1,15 @@
+/*
+ * Christopher Holmes
+ * ID: 002928626
+ * Module 5 - Program Assignment
+ * 2/11/19
+ * Abstract: Implement the selection sort algorithm to sort an array passed in
+ */
+
 #include <iostream>
 using namespace std;
 
+//Simple swap function to swap two integers in an array
 void swap(int *ap, int *bp)
 {
 	int temp = *ap;
@@ -8,24 +17,30 @@ void swap(int *ap, int *bp)
 	*bp = temp;
 }
 
+//Implementation of selection sort
 void selectionSort(int unsorted[], int arraySize)
 {
+	//Min number in cycle
 	int min;
-	//int arraySize = sizeof(unsorted)/sizeof(unsorted[0]);
+	//For loop to cycle through entire array
 	for(int i=0; i < arraySize-1; i++)
 	{
 		min = i;
+		//For loop to cycle through i+1 to end of array
 		for(int j=i+1; j < arraySize; j++)
 		{
+			//If current element in loop is less than current minimum, chan minmum
 			if(unsorted[j] < unsorted[min])
 			{
 				min = j;
 			}
 		}
+		//Swap current with smallest
 		swap(&unsorted[min], &unsorted[i]);
 	}
 }
 
+//Print array function
 void printArray(int array[], int arraySize)
 {
 	for(int i=0; i<arraySize; i++)
@@ -39,6 +54,8 @@ int main()
 {
 	int array[] = {4, 6, 8, 15, 20, 22, 10, 3, 9, 2};
 	int arraySize = sizeof(array)/sizeof(array[0]);
+	cout << "Input Array: " << endl;
+	printArray(array, arraySize);
 	selectionSort(array, arraySize);
 	cout << "Sorted Array: " << endl;
 	printArray(array, arraySize);
--- CST370/Module5/ProgramAssignment/2/selectionsort.cpp
+++ CST370/Module5/ProgramAssignment/2/selectionsort.cpp
@@ -1,6 +1,15 @@
+/*
+ * Christopher Holmes
+ * ID: 002928626
+ * Module 5 - Program Assignment
+ * 2/11/19
+ * Abstract: Implement selectionSort to run in O(nk) and use O(nk) function to calculate median
+ */
+
 #include <iostream>
 using namespace std;
 
+//Simple swap function to swap two integers in an array
 void swap(int *ap, int *bp)
 {
 	int temp = *ap;
@@ -8,13 +17,17 @@ void swap(int *ap, int *bp)
 	*bp = temp;
 }
 
+//Implementation of selection sort to run O(nk)
 void selectionSort(int unsorted[], int arraySize, int k)
 {
+	//Minimum value in cycle
 	int min;
-	//int arraySize = sizeof(unsorted)/sizeof(unsorted[0]);
+
+	//Cycle through entire array once up till k
 	for(int i=0; i < k; i++)
 	{
 		min = i;
+		//Cycle through i+1 to end of array
 		for(int j=i+1; j < arraySize; j++)
 		{
 			if(unsorted[j] < unsorted[min])
@@ -22,10 +35,12 @@ void selectionSort(int unsorted[], int arraySize, int k)
 				min = j;
 			}
 		}
+		//Swap of smallest integer to sort array
 		swap(&unsorted[min], &unsorted[i]);
 	}
 }
 
+//Function to print the entire array that takes in an array and the size of the array
 void printArray(int array[], int arraySize)
 {
 	for(int i=0; i<arraySize; i++)
@@ -37,28 +52,33 @@ void printArray(int array[], int arraySize)
 
 int main()
 {
+	//Declare variables and arrays for use in program
 	int array[] = {4, 6, 8, 15, 20, 22, 10, 3, 9, 2};
 	int arraySize = sizeof(array)/sizeof(array[0]);
 	int middle = arraySize/2.0;
+	//int k;
 
-	//cout << "Middle: " << middle << endl;
-	//cout << "arraySize % 2: " << arraySize%2 << endl;
+	//cout << "Enter k: " << endl;
+	//cin >> k;
+
+	//Display the array that was input
+	cout << "Input Array: ";
+	printArray(array, arraySize);
+
+	//Read in a K value from user
+	//cout << "k: " << k << endl;
+	//selectionSort(array, arraySize, k);
+	//printArray(array, k);
+
+	//Call the selection sort with the middle number to make it O(nk) and display median
+	selectionSort(array, arraySize, middle+1);
 	if(arraySize%2) //Odd array size
 	{
-		selectionSort(array, arraySize, middle+1);
-		//cout << "Partial Sort: " << endl;
-		//printArray(array, arraySize);
 		cout << "Median: " << array[middle] << endl;
 	}
 	else //Even array size
 	{
-		selectionSort(array, arraySize, middle+1);
-		//cout << "Partial Sort: " << endl;
-		//printArray(array, arraySize);
-		//cout << "array[middle]: " << array[middle] << " array[middle-1]: " << array[middle-1] << endl;
 		cout << "Median: " << (array[middle]+array[middle-1])/2.0 << endl;
 	}
-	//selectionSort(array, arraySize, arraySize);
-	//cout << "Sorted Array: " << endl;
-	//printArray(array, arraySize);
+
 }
